8fe90977357c07895813fef98901072598ab2378
RusirikaFernando <thathsaranif7@gmail.com>
2024-11-15 18:27:32 +0530

    feat: Add routes for submitting scores and retrieving user info with token verification

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
  
 import { submitscore } from "../controllers/score.controller.js";
+//import {getUserProfile} from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { getUserInfo } from "../controllers/user.controller.js";
+
  
 const router = express.Router();
  
-router.post("/add-score", submitscore);
+router.post("/add-score",verifyToken, submitscore);
+router.get("/user-info",verifyToken, getUserInfo);
  
 export default router;
